c33e87990f95600174b2cb412205617bab1d1915
javiurbani <javier_urbani@hotmail.com>
2022-09-13 14:29:22 -0300

    version 1.4

--- proyecto1/Appmtv/views.py
+++ proyecto1/Appmtv/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from Appmtv.models import Familia
+from django.template import loader
 
-# Create your views here.
+def familia(self):
+    familia1 = Familia(nombre = "Leandro", edad = 35, fecha_nac = "1986-1-12")
+    familia1.save()
+
+    familia2 = Familia(nombre = "Natalin", edad = 36, fecha_nac = "1985-4-6")
+    familia2.save()
+
+    familia3 = Familia(nombre = "Susana", edad = 42, fecha_nac = "1980-5-10")
+    familia3.save()
+
+    diccionario = {
+        "name1": familia1.nombre, "age1": familia1.edad, "birth1": familia1.fecha_nac,
+        "name2": familia2.nombre, "age2": familia2.edad, "birth2": familia2.fecha_nac,
+        "name3": familia3.nombre, "age3": familia3.edad, "birth3": familia3.fecha_nac
+        }
+    
+    plantilla = loader.get_template("familiar.html")
+    document = plantilla.render(diccionario)
+
+    return HttpResponse(document)
--- proyecto1/proyecto1/urls.py
+++ proyecto1/proyecto1/urls.py
@@ -1,16 +1,7 @@
-from django.contrib import admin
-from django.urls import path
-from Proyecto1.views import vista_familiar1, vista_familiar2, vista_familiar3, plantilla_familiar1, plantilla_familiar2, plantilla_familiar3
-from AppCoder.views import familiar_1, familiar_2, familiar_3
+from django.urls import path   
+from Appmtv.views import familia
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('familiar1/<nombre>/<edad>/<fecha>', vista_familiar1), 
-    path('familiar2/<nombre>/<edad>/<fecha>', vista_familiar2),
-    path('familiar3/<nombre>/<edad>/<fecha>', vista_familiar3),
-    path('familiar-1/', plantilla_familiar1),
-    path('familiar-2/', plantilla_familiar2),
-    path('familiar-3/', plantilla_familiar3),
-    path('familiar_1/', familiar_1),
-    path('familiar_2/', familiar_2),
-    path('familiar_3/', familiar_3),
+    path('familia/', familia),
+]
