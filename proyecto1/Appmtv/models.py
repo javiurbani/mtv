f7292e755e71e4a95ed67e09a91f277300a79151
javiurbani <javier_urbani@hotmail.com>
2022-09-13 14:47:52 -0300

    version 1.5

--- proyecto1/Appmtv/models.py
+++ proyecto1/Appmtv/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 class Familia(models.Model):
-    nombre_apellido = models.CharField(max_length=40)
+    nombre = models.CharField(max_length=40)
     edad = models.IntegerField()
     fecha_nac = models.DateField()
